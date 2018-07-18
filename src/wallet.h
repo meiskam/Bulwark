fab110f327c4598c7536082d05d7009d0fdd83c9
Alex Fil <lexandros71@gmail.com>
2018-07-18 09:01:11 +0400

    Need peer true. Change stacking treshold to 200

--- src/wallet.h
+++ src/wallet.h
@@ -244,7 +244,7 @@ public:
 
         // Stake Settings
         nHashDrift = 45;
-        nStakeSplitThreshold = 2000;
+        nStakeSplitThreshold = 200;
         nHashInterval = 22;
         nStakeSetUpdateTime = 300; // 5 minutes
 
